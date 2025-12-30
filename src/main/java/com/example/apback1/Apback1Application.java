69e8935a7fe200acb54bd87098c9f06d4b5c8682
inabanote <inabagaj@gmail.com>
2025-12-30 23:24:20 +0900

    springinitializr

---
+++ src/main/java/com/example/apback1/Apback1Application.java
@@ -0,0 +1,13 @@
+package com.example.apback1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apback1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apback1Application.class, args);
+	}
+
+}
